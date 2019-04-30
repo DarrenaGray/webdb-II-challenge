1b21170ded0b7d5e5e4cde19f6f612d10778552b
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-30 16:17:09 -0400

    Knex and sqlite3 connected to server. Endpoint GET request created and returing an empty array

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+	client: 'sqlite3',
+	connection: {
+		filename: './data/lambda.sqlite3',
+	},
+	useNullAsDefault: true,
+};
+
+const db = knex(knexConfig);
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
+server.get('/', (req, res) => {
+	res.send('Connected');
+});
 // endpoints here
+
+server.get('/api/zoos', (req, res) => {
+	db('zoos')
+		.then(zoos => {
+			res.status(200).json(zoos);
+		})
+		.catch(err => {
+			console.log(err);
+		});
+});
 
 const port = 3300;
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+	console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
