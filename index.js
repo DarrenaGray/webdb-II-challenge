282e3c260c99bd0df84dc3db6569144bafb3074a
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-30 17:30:03 -0400

    Created endpoint PUT for updating existing zoos in the database

--- index.js
+++ index.js
@@ -64,6 +64,22 @@ server.post('/api/zoos', (req, res) => {
 		});
 });
 
+server.put('/api/zoos/:id', (req, res) => {
+	db('zoos')
+		.where({ id: req.params.id })
+		.update(req.body)
+		.then(updatedZoo => {
+			if (updatedZoo) {
+				res.status(200).json({ message: 'Zoo info was successfully updated.' });
+			} else {
+				res.status(404).json({ message: 'The zoo with the specified ID was not found.' });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ err, message: 'The zoo info could not be updated.' });
+		});
+});
+
 const port = 3300;
 server.listen(port, function() {
 	console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
